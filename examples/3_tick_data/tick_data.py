21a16e8ffdd8972eb9e151cc647ad840f554f6a9
JeongHoo Won <clomia.sig@gmail.com>
2024-08-07 15:51:26 +0900

    example 3 complete

--- examples/3_tick_data/tick_data.py
+++ examples/3_tick_data/tick_data.py
@@ -1,20 +1,29 @@
+import time
+from pathlib import Path
+
 import pandas as pd
-from time import sleep
 from lightweight_charts import Chart
+from lightweight_charts_server import Server, Stream
 
-if __name__ == '__main__':
+directory = Path(__file__).parent
 
-    df1 = pd.read_csv('ohlc.csv')
+df1 = pd.read_csv(directory / "ohlc.csv")
+df2 = pd.read_csv(directory / "ticks.csv")
 
-    # Columns: time | price
-    df2 = pd.read_csv('ticks.csv')
 
+def create():
     chart = Chart()
-
     chart.set(df1)
+    return chart
 
-    chart.show()
 
+def update(chart: Chart):
     for i, tick in df2.iterrows():
         chart.update_from_tick(tick)
-        sleep(0.03)
+        time.sleep(0.03)
+
+
+if __name__ == "__main__":
+    display = Stream(creator=create, updater=update)
+    server = Server(display)
+    server.serve()
