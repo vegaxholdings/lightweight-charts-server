02546d9230c9a62c5365f1d39cf09f748094d75e
JeongHoo Won <clomia.sig@gmail.com>
2024-09-06 16:02:50 +0900

    Ensure that multiprocessing's start_method is spawn

--- lightweight_charts_server/__init__.py
+++ lightweight_charts_server/__init__.py
@@ -1,3 +1,14 @@
+import multiprocessing
+
+if multiprocessing.get_start_method() != "spawn":
+    try:
+        # For use Xvfb, All child processors must inherit the Xvfb context.
+        multiprocessing.set_start_method("spawn")
+    except RuntimeError as e:
+        raise ImportError(
+            "lightweight_charts_server must be imported before lightweight_charts"
+        ) from e
+
 from lightweight_charts_server.server import Server
 from lightweight_charts_server.display import View, Stream
 from lightweight_charts_server.system import log
