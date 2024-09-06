246d0a92b5724878ab72bfc6cddf9ffa6e9a2c90
JeongHoo Won <clomia.sig@gmail.com>
2024-09-06 17:30:40 +0900

    Add a message to guide the installation of xvfb & spawn force=True

--- lightweight_charts_server/__init__.py
+++ lightweight_charts_server/__init__.py
@@ -4,19 +4,33 @@ import platform
 import subprocess
 import multiprocessing
 
+xvfb_error_message = """ 
+Error: Xvfb is not installed or unavailable!
+
+Please set up the Xvfb environment by installing the necessary packages using the following commands:
+
+# Install Xvfb
+apt install -y xvfb
+
+# Install GTK (GUI Toolkit)
+apt install -y python3-gi libgtk-3-dev gir1.2-webkit2-4.1
+
+# Install the package that binds GTK to Python
+apt install -y libgirepository1.0-dev 
+pip install pygobject
+"""
 
 if platform.system() == "Linux" and not os.environ.get("DISPLAY"):
-    subprocess.run(["xvfb-run", "-a", "python"] + sys.argv)
-    sys.exit()
+    try:
+        subprocess.run(["xvfb-run", "-a", "python"] + sys.argv)
+    except FileNotFoundError:
+        print(xvfb_error_message)
+    finally:
+        sys.exit()
 
 if multiprocessing.get_start_method() != "spawn":
-    try:
-        # If use Xvfb, All child processors must inherit the Xvfb context.
-        multiprocessing.set_start_method("spawn")
-    except RuntimeError as e:
-        raise ImportError(
-            "lightweight_charts_server must be imported before lightweight_charts"
-        ) from e
+    # If use Xvfb, All child processors must inherit the Xvfb context.
+    multiprocessing.set_start_method("spawn", force=True)
 
 
 from lightweight_charts_server.server import Server
