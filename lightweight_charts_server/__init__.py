d5af41a7d2f45dd5aca4f6984764e5c5a3660c77
JeongHoo Won <clomia.sig@gmail.com>
2024-09-06 16:52:14 +0900

    It will now automatically use xvfb.

--- lightweight_charts_server/__init__.py
+++ lightweight_charts_server/__init__.py
@@ -1,13 +1,23 @@
+import os
+import sys
+import platform
+import subprocess
 import multiprocessing
+
+
+if platform.system() == "Linux" and not os.environ.get("DISPLAY"):
+    subprocess.run(["xvfb-run", "-a", "python"] + sys.argv)
+    sys.exit()
 
 if multiprocessing.get_start_method() != "spawn":
     try:
-        # For use Xvfb, All child processors must inherit the Xvfb context.
+        # If use Xvfb, All child processors must inherit the Xvfb context.
         multiprocessing.set_start_method("spawn")
     except RuntimeError as e:
         raise ImportError(
             "lightweight_charts_server must be imported before lightweight_charts"
         ) from e
+
 
 from lightweight_charts_server.server import Server
 from lightweight_charts_server.display import View, Stream
